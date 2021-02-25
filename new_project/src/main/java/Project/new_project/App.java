0a10c30d0a4ee35d95a2b89b47144516699707e4
SONALISINGH1998 <sonalisingh4798@gmail.com>
2021-02-24 23:51:45 -0800

    first commit

---
+++ new_project/src/main/java/Project/new_project/App.java
@@ -0,0 +1,13 @@
+package Project.new_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
